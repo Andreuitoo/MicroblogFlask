7c31587a871dbaf71d1775e057e0e15462bcba42
Andreuitoo <andreu77762@gmail.com>
2023-09-15 13:02:15 +0200

    chapter 21 completed

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ from time import time
 import jwt
 from flask import current_app
 from app.search import add_to_index, remove_from_index, query_index
+import json
 
 
 class SearchableMixin(object):
@@ -71,6 +72,7 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    notifications = db.relationship('Notification', backref='user', lazy='dynamic')
 
     followed = db.relationship(
         'User', secondary=followers,
@@ -130,6 +132,12 @@ class User(UserMixin, db.Model):
         last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
         return Message.query.filter_by(recipient=self).filter(
             Message.timestamp > last_read_time).count()
+    
+    def add_notification(self, name, data):
+        self.notifications.filter_by(name=name).delete()
+        n = Notification(name=name, payload_json=json.dumps(data), user=self)
+        db.session.add(n)
+        return n
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -156,3 +164,14 @@ class Message(db.Model):
 
     def __repr__(self):
         return '<Message {}>'.format(self.body)
+    
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), index=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    timestamp = db.Column(db.Float, index=True, default=time)
+    payload_json = db.Column(db.Text)
+
+    def get_data(self):
+        return json.loads(str(self.payload_json))
