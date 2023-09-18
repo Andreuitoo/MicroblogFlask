3c6d3328971fb9b64c2b5716df152eb2e34db92e
Andreuitoo <andreu77762@gmail.com>
2023-09-18 11:36:08 +0200

    cap 22

--- config.py
+++ config.py
@@ -16,9 +16,10 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['andreu77763@gmail.com']
+    ADMINS = ['andreu.sadurni@etoolinnovation.com']
     LANGUAGES = ['en', 'es']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
     POSTS_PER_PAGE = 25
+    REDIS_URL = os.environ.get('REDIS_URL') or 'redis://'
 
