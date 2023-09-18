9eec8bd0b4b91f96e4c2f0cad65a598b138c7906
Andreuitoo <andreu77762@gmail.com>
2023-09-18 13:20:21 +0200

    ultimo cap

--- app/__init__.py
+++ app/__init__.py
@@ -40,6 +40,9 @@ def create_app(config_class=Config):
         if app.config['ELASTICSEARCH_URL'] else None
     app.redis = Redis.from_url(app.config['REDIS_URL'])
     app.task_queue = rq.Queue('microblog-tasks', connection=app.redis)
+    
+    from app.api import bp as api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
